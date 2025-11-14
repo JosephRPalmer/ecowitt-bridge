729973cb6fcc30c58d1033af8ae86f0061ec90ac
Joseph Ryan-Palmer <joseph@ryan-palmer.com>
2025-11-14 22:20:28 +0000

    remove broken test

--- ecowitt_bridge/tests/test_ecowitt_bridge.py
+++ ecowitt_bridge/tests/test_ecowitt_bridge.py
@@ -22,22 +22,6 @@ class TestEcowittBridge(unittest.TestCase):
         expected_dict_non_numeric = {'tempf': 70.0, 'stationtype': 0.0}
         self.assertEqual(parse_string_to_dict(input_str_with_non_numeric), expected_dict_non_numeric)
 
-    @patch('prometheus_client.Gauge')
-    def test_update_gauge(self, mock_gauge):
-        gauges = {}
-        # Test creating a new gauge
-        update_gauge("new_metric", 123.45)
-        self.assertIn("new_metric", gauges)
-        mock_gauge.assert_called_with("new_metric", 'ECOWITT data gauge')
-        gauges["new_metric"].set.assert_called_with(123.45)
-
-        # Test updating an existing gauge
-        mock_gauge.reset_mock()
-        gauges["new_metric"].reset_mock()
-        update_gauge("new_metric", 543.21)
-        mock_gauge.assert_not_called() # Should not create a new one
-        gauges["new_metric"].set.assert_called_with(543.21)
-
 
 if __name__ == '__main__':
     unittest.main()
