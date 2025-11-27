594b4479d7ce7bee88a2dadf9ab9b7e1f4f9a12f
Joseph Ryan-Palmer <joseph@ryan-palmer.com>
2025-11-27 19:49:40 +0000

    testing

--- ecowitt_bridge/ecowitt_bridge.py
+++ ecowitt_bridge/ecowitt_bridge.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
-logging.basicConfig(level=settings.loglevel if settings.loglevel in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] else 'DEBUG',
+logging.basicConfig(level='DEBUG',
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 
