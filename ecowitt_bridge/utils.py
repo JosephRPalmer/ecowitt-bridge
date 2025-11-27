57848a57e1426c969f6c4d9b075d1bc89d639475
Joseph Ryan-Palmer <joseph@ryan-palmer.com>
2025-11-27 18:38:24 +0000

    rework and add pydantic

---
+++ ecowitt_bridge/utils.py
@@ -0,0 +1,30 @@
+from logging import logging
+
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+def parse_string_to_dict(input_string):
+    datapoints = {}
+    pairs = input_string.replace("[", "").replace(
+        "'", "").replace("]", "").split('&')
+
+    for pair in pairs:
+        key, value = pair.split('=')
+        try:
+            datapoints[key] = float(value)
+        except ValueError:
+            datapoints[key] = 0.0
+            logging.warning("Non-numeric value for key {}: {}".format(key, value))
+
+    return datapoints
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
+
+
+def in_to_hpa(ins):
+    hpa = ins * 33.6585
+    return hpa
